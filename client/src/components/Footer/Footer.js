7e71cd32f53909a21fe53456bc2ec1a0a375698b
Serhii Kolesnyk <morepositivecode@gmail.com>
2026-01-19 11:34:50 +0200

    added referral and social links

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { REFERRAL_LINK, SOCIAL_LINK } from '../../constants/social';
 import './footer.scss';
 
 const Footer = () => {
@@ -11,33 +12,42 @@ const Footer = () => {
           <Col>
             <div>Some of the information above is taken from sources like these</div>
             <div className="exchanges">
-              <a href="https://binance.com/" target="_blank" rel="noreferrer">
+              <a href={REFERRAL_LINK.BINANCE} target="_blank" rel="noreferrer">
                 <img src="https://bin.bnbstatic.com/static/images/common/favicon.ico" alt="Binance" />
               </a>
-              <a href="https://www.okx.com/" target="_blank" rel="noreferrer">
+              <a href={REFERRAL_LINK.OKX} target="_blank" rel="noreferrer">
                 <img src="https://www.okx.com/favicon.ico" alt="OKX" />
               </a>
-              <a href="https://www.huobi.com/" target="_blank" rel="noreferrer">
+              <a href={REFERRAL_LINK.HUOBI} target="_blank" rel="noreferrer">
                 <img src="https://www.huobi.com/favicon.ico" alt="Huobi" />
               </a>
-              <a href="https://www.gate.io/" target="_blank" rel="noreferrer">
+              <a href={REFERRAL_LINK.GATE} target="_blank" rel="noreferrer">
                 <img src="https://www.gate.io/favicon.ico?v=2.0" alt="Gate" />
               </a>
             </div>
           </Col>
           <Col>
             <div className="social">
-              facebook, twitter, telegram
+              <div>Contact us:</div>
+              <a href={SOCIAL_LINK.FACEBOOK} target="_blank" rel="noreferrer">
+                <img src="https://www.facebook.com/favicon.ico" alt="facebook" />
+              </a>
+              <a href={SOCIAL_LINK.TWITTER} target="_blank" rel="noreferrer">
+                <img src="https://www.twitter.com/favicon.ico" alt="twitter" />
+              </a>
+              <a href={SOCIAL_LINK.TELEGRAM} target="_blank" rel="noreferrer">
+                <img src="https://www.telegram.com/favicon.ico" alt="telegram" />
+              </a>
             </div>
           </Col>
-          <Col className="text-end">
+        </Row>
+        <Col className="text-end">
             <ul className="col menu">
               <li><Link to="/about">About</Link></li>
               <li><Link to="/support">Support</Link></li>
             </ul>
             All right reserved, 2022
           </Col>
-        </Row>
       </Container>
     </footer>
   );
---
+++ client/src/constants/social.js
@@ -0,0 +1,12 @@
+export const SOCIAL_LINK = {
+  FACEBOOK: 'https://www.facebook.com',
+  TWITTER: 'https://www.twitter.com',
+  TELEGRAM: 'https://www.telegram.com',
+}
+
+export const REFERRAL_LINK = {
+  BINANCE: 'https://accounts.binance.com/ru/register?ref=39975583',
+  OKX: 'https://www.okx.com/join/2927657',
+  HUOBI: 'https://www.huobi.ug/en-us/v/register/double-invite/?invite_code=f8rb3223&name=&avatar=&inviter_id=11345710',
+  GATE: 'https://www.gate.io/signup/3830980',
+};

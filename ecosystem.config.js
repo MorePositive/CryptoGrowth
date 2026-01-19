86bef587a912c1dafe65e31812c7cf7f61b83cb8
Serhii Kolesnyk <morepositivecode@gmail.com>
2026-01-19 11:34:51 +0200

    updating paths..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     apps : [{
         name: "cryptogrowth",
-        script: "~/cryptogrowth/server/index.js",
+        script: "./server/index.js",
         env: {
             NODE_ENV: "production"
         }
     }, {
         name: "daily job",
-        script: "~/cryptogrowth/server/jobs/daily.js",
+        script: "./server/jobs/daily.js",
         cron_restart: "0 1 * * *"
     }]
 };
